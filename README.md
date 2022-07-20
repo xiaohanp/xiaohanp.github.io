7f41afe9e50601def448860ccc2bc75d96b93388
pxh <xiaohanp@kth.se>
2022-07-20 11:18:36 +0200

    routing issue

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+---
+permalink: /index.html
+---
+
 ## Project setup
 ```
 npm install
--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     },
   },
   transpileDependencies: ['quasar'],
-  publicPath: process.env.NODE_ENV === "production" ? "/xiaohanp.github.io/src" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/xiaohanp.github.io/" : "/",
 },
 );
